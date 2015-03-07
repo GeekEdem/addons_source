776083b0f1111811ca2a07cad5d8e92557f56d37
GeekEdem <4ixpar@gmail.com>
2015-03-08 01:07:33 +0200

    Release

--- plugin.video.megogo/start.py
+++ plugin.video.megogo/start.py
@@ -10,13 +10,33 @@ addon_version	= addon.getAddonInfo('version')
 addon_path 		= xbmc.translatePath(addon.getAddonInfo('path'))
 language        = addon.getLocalizedString
 
+source = 'http://raw.github.com/GeekEdem/zip/master/plugin.video.megogo/addon.xml'
+
 sys.path.append(os.path.join(addon_path, 'resources', 'lib'))
 from Utils import fetch_data
 
 xbmc.log('[%s]: Start plugin! Version: %s' % (addon_name, addon_version))
+
 ####################################		Start Splash	#########################################
 splash = xbmcgui.WindowXML('splash.xml', addon_path)
 splash.show()
+
+xbmc.log('[%s]: Trying to get new version...' % addon_name)
+try:
+    request = urllib2.Request(url=source, headers={'User-Agent': 'MEGOGO Addon for XBMC/Kodi'})
+    request = urllib2.urlopen(request)
+    http = request.read()
+    request.close()
+
+    p = re.compile('name="MEGOGO\WNET"\W.*?version="([^"]*?)"')
+    branch_release = re.search(p, http).group(1)
+
+    if addon_version != branch_release:
+        dialog = xbmcgui.Dialog()
+        dialog.ok(language(1033), language(1034))
+except:
+    xbmc.log('[%s]: No new version addon available.' % addon_name)
+    pass
 
 if fetch_data(page='configuration'):    # Get config from MEGOGO
     import default
@@ -25,7 +45,7 @@ else:
     dialog = xbmcgui.Dialog()
     dialog.ok(language(1025), language(1031), language(1032))
     dialog.close()
-    
+
 splash.close()
 
-xbmc.log('[%s]: Close plugin! Version: %s' % (addon_name, addon_version))
+xbmc.log('[%s]: Close plugin. Version: %s' % (addon_name, addon_version))
